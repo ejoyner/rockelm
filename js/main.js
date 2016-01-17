28c015f215ec126b57253556dda4f691f2e4b344
ejoyner <ejoyner@Vishals-MacBook-Pro.local>
2016-01-17 15:07:41 -0500

    First Committ

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+
+  $('.carousel').slick({
+   	 slidesToShow: 1,
+  	 slidesToScroll: 1,
+  	 autoplay: true,
+  	 autoplaySpeed: 5000
+  });
+
+
+$('.burger').click(function(e) {
+	$('.menu').slideToggle('slow'); 
+	//console.log('you')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
